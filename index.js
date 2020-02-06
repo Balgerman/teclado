b92e7cf6ade6a6fd5b462a4318d07ab57e7a264f
German <balgerman96@gmail.com>
2020-02-06 01:49:36 -0500

    teclado terminado

--- index.js
+++ index.js
@@ -1,67 +1,12 @@
-class Pianoroll {
-  constructor(tipo) {
-    this.tipo = tipo;
-  }
+var context = new AudioContext();
 
-  reproducirDo() {
-    document.getElementById("doN").play();
-  }
-
-  reproducirRe() {
-    var Re = document.getElementById("doA").onkeydown;
-    return Re;
-  }
-  reproducirMi() {
-    var Mi = this.notas[2];
-    return;
-  }
-  reproducirFa() {
-    var Fa = this.notas[3];
-    return;
-  }
-  reproducirSol() {
-    var Sol = this.notas[4];
-    return;
-  }
-  reproducirLa() {
-    var La = this.notas[5];
-    return;
-  }
-  reproducirSi() {
-    var Si = this.notas[6];
-    return;
-  }
+function jsNota(frecuencia) {
+  let o = context.createOscillator();
+  g = context.createGain();
+  o.connect(g);
+  o.type = "sawtooth";
+  o.frequency.value = frecuencia;
+  g.connect(context.destination);
+  o.start(0);
+  g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 1.5);
 }
-
-const piano = new Pianoroll();
-
-document.addEventListener("keydown", function(event) {
-  let tecla = event.keyCode;
-  tecla;
-  console.log(tecla);
-
-  switch (tecla) {
-    case 65:
-      console.log("Do");
-      break;
-
-    case 83:
-      console.log("Re");
-      break;
-    case 68:
-      console.log("Mi");
-      break;
-    case 70:
-      console.log("FA");
-      break;
-    case 71:
-      console.log("SOL");
-      break;
-    case 72:
-      console.log("LA");
-      break;
-    case 74:
-      console.log("SI");
-      break;
-  }
-});
